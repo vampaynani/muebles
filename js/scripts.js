fefeec24d1c07b601e65db03eb3d73088faf121c
Wenceslao Negrete <vampediem@gmail.com>
2013-09-20 00:56:07 -0500

    Draggable enabled

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,55 @@
 
 $( document ).ready(function() {
+	/* Init */
+	for (var i = 1; i <= 39; i++) {
+    	var imagen = $('<img>').attr({
+	    	src:'img/'+getZero(i)+'.png',
+	     	id:'modulo'+i,
+	     	class: 'modulos',
+	     	draggable: 'true' 
+	     });  
+    	imagen.on('dragstart', saveData);
+		$("#selector").append(imagen);
+	};
+	$('#closet div').on('dragover', enableDrop);
+	$('#closet div').on('drop', checkCollision);
 
-		for (var i = 1; i <= 39; i++) {
-    	    var imagen = $('<img>').attr({
-	     		src:'img/'+getZero(i)+'.png',
-	     		id:'modulo'+i,
-	     		class: 'modulos' 
-	     	});  
-			$("#selector").append(imagen);
-		};
-
-		function getZero(n){
-			if (n.toString().length <2) {
-				return '0'+ n
-			}else{
-				return n
-			};
-		}
-
-		$('.slider').slider().on('slide', function(ev){
-			var modulosSlider = $('#sliderCloset').val();
-			var modulosTotal = $('#closet').find('img').length;
-			console.log(modulosTotal + " " + modulosSlider);
+	/* Listeners */
+	$('.slider').slider().on('slide', function(ev){
+		var modulosSlider = $('#sliderCloset').val();
+		var modulosTotal = $('#closet').find('img').length;
+		console.log("t:"+modulosTotal + " s:" + modulosSlider);
  
-			if (modulosTotal <= modulosSlider) {
-				for (var i = modulosTotal; i <= modulosSlider-1; i++) {
+		if (modulosTotal <= modulosSlider) {
+			for (var i = modulosTotal; i <= modulosSlider-1; i++) {
 				$("#closet").append("<div><img src='img/01.png'/></div>");
-				};
+			};
+		}else if(modulosTotal > modulosSlider){
+ 			$("#closet div").last().remove(); 
+  		};  
+  		$('#closet div').on('dragover', enableDrop);
+		$('#closet div').on('drop', checkCollision); 
+	});
 
- 			}else if(modulosTotal > modulosSlider){
- 				$("#closet div").last().remove(); 
-  			};   
-
-		});
-
-		$('.modulos').click(function() {
-			
-		});
-
+	/* Functions */
+	var imgsrc = '';
+	function saveData(e){
+		imgsrc = $(this).attr('src');
+		console.log(imgsrc);
+	}
+	function enableDrop(e){
+		if(e.preventDefault) e.preventDefault();
+		return false;
+	}
+	function checkCollision(e){
+		var img = $(this).find('img')[0];
+		$(img).attr('src', imgsrc);
+	}
+	function getZero(n){
+		if (n.toString().length <2) {
+			return '0'+ n
+		}else{
+			return n
+		};
+	}
 });
